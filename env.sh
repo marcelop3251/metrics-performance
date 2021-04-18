0342dbedee94e3b8046fff9178a3a1b9d7f5268e
marcelo.palma <marcelo.palma@ztech.net>
2021-04-18 18:43:42 -0300

    feat: add projeto kotlin, grafana, prometheus docker-compose and performance test

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export DATABASE_JDBC_URL=jdbc:postgresql://localhost:5432/postgres
+export DATABASE_USER_NAME=postgres
+export DATABASE_PASSOWORD=postgres
+export DATABASE_DRIVER=org.postgresql.Driver
+export POOL=10
